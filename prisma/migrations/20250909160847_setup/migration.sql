c8719d436d63691cc9aa83e3a5663d9989ca5184
Alexis <awaknocode@gmail.com>
2025-09-09 18:08:58 +0200

    feat: Add initial migration for User and Recipe tables with constraints and indexes

--- prisma/migrations/20250909160847_setup/migration.sql
+++ prisma/migrations/20250909160847_setup/migration.sql
@@ -9,5 +9,16 @@ CREATE TABLE "public"."User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "public"."Recipe" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
